5b27070b6da9eacd2fc7fdab7c6b11a9174d7177
Gleb Goloborodko <glbgoloborodko@yandex.ru>
2023-08-29 14:04:07 +0300

    Complete first

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,10 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+def test_item():
+    some_item = Item('тест', 15000, 5)
+    assert some_item.calculate_total_price() == 75000
+
+def test_discount():
+    smartphone = Item('Samsung', 40000, 60)
+    assert smartphone.apply_discount() == smartphone.price
