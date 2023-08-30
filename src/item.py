a139540a5ccee3239f11c4b6c42846452f2e10ea
Gleb Goloborodko <glbgoloborodko@yandex.ru>
2023-08-30 23:27:46 +0300

    Complete second

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +15,28 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    def __str__(self):
+        return (f'{self.__name}\n'
+                f'Цена {self.price}\n'
+                f'Кол-во {self.quantity}\n'
+                f'---------------')
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) <= 10:
+            self.__name = new_name
+
+        else:
+            self.__name = new_name[:10]
 
     def calculate_total_price(self) -> float:
         """
@@ -32,3 +52,27 @@ class Item:
         """
         self.price *= Item.pay_rate
         return self.price
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        items = []
+        cls.all.clear()
+        with open(path, 'r', newline='', encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = row['name']
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                item = cls(name, price, quantity)
+                items.append(item)
+        return items
+
+    @staticmethod
+    def string_to_number(str):
+        if '.' in str:
+            str_float = float(str)
+            str_int = int(str_float)
+        else:
+            str_int = int(str)
+        return str_int
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,4 +1,6 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
 from src.item import Item
 
 def test_item():
@@ -8,3 +10,18 @@ def test_item():
 def test_discount():
     smartphone = Item('Samsung', 40000, 60)
     assert smartphone.apply_discount() == smartphone.price
+
+def test_csv():
+    some_item = Item('тест', 15000, 5)
+    some_item.instantiate_from_csv('/Users/ttkmb/WORKHARD/V2.0/electronic_shop/src/items.csv')
+    assert len(Item.all) == 5
+
+def test_str():
+    some_item = Item('тест', 15000, 5)
+    assert some_item.string_to_number('67.45') == 67
+
+def test_len_name():
+    some_item = Item('12345678910111213', 15000, 5)
+    with pytest.raises(AssertionError):
+        assert some_item.name == '1234567891'
+
